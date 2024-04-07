24e6a20e1e6118e247e21d3470f649b27d7a4b38
Rutik176 <118980215+Rutik176@users.noreply.github.com>
2024-04-08 00:19:30 +0530

    Add files via upload

---
+++ DAY 24 TUPLES .py
@@ -0,0 +1,12 @@
+tup =(1,4,4,)
+#tup[0] =90
+print(type(tup),tup)
+print(len(tup))
+print(tup[0])
+print(tup[-1])
+print(tup[2])
+
+if 342 in tup:
+    print("yes")
+else:
+    print ("no ")
---
+++ day 23 list methods .py
@@ -0,0 +1,15 @@
+l= [12,34,5,5,3,8,9]
+print(l)
+#l.append(5)
+#l.sort(reverse=True)
+#l.reverse()
+#print(l.index(1))
+#print(l.count(1))
+#m= l.copy()
+#m[0]=0
+l.insert(1,899)
+m= [900,1000,1200]
+k= l+m
+#print(K)
+#l.extend(m)
+print(l)
