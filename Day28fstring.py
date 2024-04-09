6a243a561307cd5915044cd4bb85ab992f49e062
Rutik176 <118980215+Rutik176@users.noreply.github.com>
2024-04-09 23:19:14 +0530

    Add files via upload

---
+++ Day28fstring.py
@@ -0,0 +1,11 @@
+letter = "hey my name is {} ans in mt brain"
+country ="india"
+name= "rutik"
+
+print(letter.format(country,name))
+print(f"hey my name is {name} ans in mt brafor in{country}")
+
+
+price=585.5342
+txt = f"for only {price:.3f}dollars!"
+print(txt)
